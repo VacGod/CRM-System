9f9f8ab72fe0d5246ee05b3a858aff381648adc2
VacGod <143494256+VacGod@users.noreply.github.com>
2024-03-12 09:43:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ $servername = "localhost";
 $username = "root"; 
 $password = ""; 
 $dbname = "crmv4";
-
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
